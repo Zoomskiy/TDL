fddc0b2db13822c5c27afd7d1892d10bd5dc54ce
Alexander Sindetckiy <zoomskiy@gmail.com>
2021-02-01 22:45:22 +0300

    зкуз

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import {Meta, Story} from "@storybook/react";
 import AppWithRedux from "../AppWithRedux";
 import {ReduxStoreProviderDecorator} from "./decorators/ReduxStoreProviderDecorator";
-
 export default {
     title: "Todolist/AppWithRedux",
     component: AppWithRedux,
     decorators: [ReduxStoreProviderDecorator]
 }as Meta
-
 const Template: Story = () => <AppWithRedux/>
-
 export const AppWithReduxExample = Template.bind({})
 AppWithReduxExample.args = {}
